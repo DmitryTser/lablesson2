65a4eca5fc145610ff76845f1e1c5abf1d96dd1f
DmitryTser <dimka.cer@gmail.com>
2019-12-19 22:35:21 +0300

    Initial commit

---
+++ diagapp/src/main/java/ru/mirea/tsertsenyuk/diagapp/MainActivityDialog.java
@@ -0,0 +1,48 @@
+package ru.mirea.tsertsenyuk.diagapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityDialog extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dialog);
+    }
+    public void onClickShowTimeDialog(View view) {
+       // MyAlertDialogFragment dialogFragment = new MyAlertDialogFragment ();
+       // dialogFragment.show(getSupportFragmentManager(), "dialog");
+        MyTimeDialogFragment timeDialog = new MyTimeDialogFragment();
+        timeDialog.show(getSupportFragmentManager(), "dialog");
+        /*TextView tvTime = (TextView) findViewById(R.id.tvTime);
+        if timeDialog.
+        tvTime.setText(timeDialog.getTime());*/
+        //Toast.makeText(getApplicationContext(), timeDialog.getTime(), Toast.LENGTH_LONG).show();
+
+    }
+    public void onClickShowDateDialog(View view) {
+        MyDateDialogFragment dateDialog = new MyDateDialogFragment();
+        dateDialog.show(getSupportFragmentManager(), "dialog");
+    }
+
+    public void onClickPD1 (View view)
+    {
+        MyProgressDialog prDialog = new MyProgressDialog();
+        prDialog.show(getSupportFragmentManager(), "dialog");
+    }
+
+    public void onOkClicked() {
+    Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
+                   Toast.LENGTH_LONG).show();
+    }
+    public void onCancelClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Нет\"!",
+                Toast.LENGTH_LONG).show();
+    }
+    public void onNeutralClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
+                Toast.LENGTH_LONG).show();
+    }
+}
